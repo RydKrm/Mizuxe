6e7b4a0a816b98d631d976504e6a40c0fb5c554d
RydKrm <alriyadkarim@gmail.com>
2024-01-23 13:38:48 +0600

    Figma to Html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+//   $(".authors-body").scroll(function(){
+//     //   $(".authors-animate").slideDown({
+//     //     width:'300px',
+//     //     opacity: '0.9'
+//     //   });
+//     // $(".authors-animate").fadeOut();
+//     $(".card").hide();
+//   });
+
+  $("div").click(function(){
+    $(".card").animate({
+        height: '300px',
+        fontSize: '50px'
+        
+    });
+ 
+});
+
+
+
+});
